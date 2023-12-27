0d411976f7c5c5e31443704ca2c3355a9f195b03
sphilipakis <380192+sphilipakis@users.noreply.github.com>
2023-12-27 10:04:25 -0800

    Add missing package

--- Package.swift
+++ Package.swift
@@ -24,13 +24,17 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0" ),
+        .package(url: "https://github.com/pointfreeco/swift-concurrency-extras", from: "1.1.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "LLMKit",
-            dependencies: ["LLMToolMacros"]
+            dependencies: [
+                "LLMToolMacros",
+                .product(name: "ConcurrencyExtras", package: "swift-concurrency-extras"),
+            ]
         ),
         .testTarget(
             name: "LLMKitTests",
